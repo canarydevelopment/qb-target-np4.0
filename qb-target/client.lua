2eb28f44c8cfe264f98aac112b5ed8b0992125c1
Canary Development <95531512+canarydevelopment@users.noreply.github.com>
2024-02-02 22:26:34 +0200

    Update client.lua

--- qb-target/client.lua
+++ qb-target/client.lua
@@ -64,35 +64,28 @@ end
 -- Functions
 
 local function DrawTarget()
-	CreateThread(function()
-		while not HasStreamedTextureDictLoaded("shared") do Wait(10) RequestStreamedTextureDict("shared", true) end
-		local sleep
-		local r, g, b, a
-		while targetActive do
-			sleep = 500
-			for _, zone in pairs(listSprite) do
-				sleep = 0
-
-				r = zone.targetoptions.drawColor?[1] or Config.DrawColor[1]
-				g = zone.targetoptions.drawColor?[2] or Config.DrawColor[2]
-				b = zone.targetoptions.drawColor?[3] or Config.DrawColor[3]
-				a = zone.targetoptions.drawColor?[4] or Config.DrawColor[4]
-
-				if zone.success then
-					r = zone.targetoptions.successDrawColor?[1] or Config.SuccessDrawColor[1]
-					g = zone.targetoptions.successDrawColor?[2] or Config.SuccessDrawColor[2]
-					b = zone.targetoptions.successDrawColor?[3] or Config.SuccessDrawColor[3]
-					a = zone.targetoptions.successDrawColor?[4] or Config.SuccessDrawColor[4]
-				end
-
-				SetDrawOrigin(zone.center.x, zone.center.y, zone.center.z, 0)
-				DrawSprite("shared", "emptydot_32", 0, 0, 0.02, 0.035, 0, r, g, b, a)
-				ClearDrawOrigin()
-			end
-			Wait(sleep)
-		end
-		listSprite = {}
-	end)
+    CreateThread(function()
+           while not HasStreamedTextureDictLoaded("targets") do Wait(10) 
+   		   RequestStreamedTextureDict("targets", true) end
+           local sleep
+           local r, g, b, a
+           while targetActive do
+               sleep = 500
+               for _, zone in pairs(listSprite) do
+                   sleep = 0
+                   SetDrawOrigin(zone.center.x, zone.center.y, zone.center.z, 0)
+                   if zone.success then
+                       DrawSprite("targets", "target_success", 0, 0, 0.02, 0.035, 0, 255, 255, 255, 255)
+                   else 
+                       DrawSprite("targets", "target_search", 0, 0, 0.02, 0.035, 0, 255, 255, 255, 255)
+                   end
+   
+                   ClearDrawOrigin()
+               end
+               Wait(sleep)
+           end
+        listSprite = {}
+    end)
 end
 
 local function RaycastCamera(flag, playerCoords)
